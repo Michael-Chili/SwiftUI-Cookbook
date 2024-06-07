d4c03b2092696a4e3f9ffa5abe14d7969235b4f9
chili <michael.chili.dev@gmail.com>
2024-06-07 19:25:38 +0200

    Chapter 01: Added initial version of Hello World

---
+++ chapter-01/HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Michael Chili on 07/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
